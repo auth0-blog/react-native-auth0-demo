662862e5ba3976944d0c99b466690afb068690b2
Juan Cruz Martinez <bajcmartinez@gmail.com>
2023-04-21 09:50:27 +0200

    Add Auth0 to an expo application

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {useAuth0, Auth0Provider} from 'react-native-auth0';
+import {StyleSheet, Text, View, Button, Alert} from 'react-native';
+import Constants from 'expo-constants';
+
+const customScheme = 'auth0-rn-demo';
+
+const Home = () => {
+  const {authorize, clearSession, user, error, getCredentials} = useAuth0();
+
+  const onLogin = async () => {
+    try {
+      await authorize({
+        scope: 'openid profile email',
+        audience: Constants.expoConfig.extra.auth0Audience
+      }, {
+        customScheme: customScheme
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const loggedIn = user !== undefined && user !== null;
+
+  const onLogout = async () => {
+    try {
+      await clearSession({customScheme: customScheme});
+    } catch (e) {
+      console.log('Log out cancelled');
+    }
+  };
+
+  const onCallAPI = async () => {
+    const accessToken = (await getCredentials()).accessToken;
+    const apiResponse = await fetch(`${Constants.expoConfig.extra.apiURL}/api/messages/protected`, {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+    Alert.alert(await apiResponse.text());
+  }
+
+  return (
+      <View style={styles.container}>
+        <Text style={styles.header}> Auth0 React Native - Login </Text>
+        {user && <Text>You are logged in as {user.name}</Text>}
+        {!user && <Text>You are not logged in</Text>}
+        {error && <Text>{error.message}</Text>}
+
+        {user && <Button title="Call API" onPress={onCallAPI} />}
+
+        <Button
+            onPress={loggedIn ? onLogout : onLogin}
+            title={loggedIn ? 'Log Out' : 'Log In'}
+        />
+      </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Auth0Provider domain={Constants.expoConfig.extra.auth0Domain} clientId={Constants.expoConfig.extra.auth0ClientId}>
+      <Home />
+    </Auth0Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
 });
--- app.config.js
+++ app.config.js
@@ -1,4 +1,6 @@
-{
+import 'dotenv/config';
+
+export default {
   "expo": {
     "name": "react-native-auth0-demo",
     "slug": "react-native-auth0-demo",
@@ -6,6 +8,12 @@
     "orientation": "portrait",
     "icon": "./assets/icon.png",
     "userInterfaceStyle": "light",
+    "extra": {
+      "auth0Domain": process.env.AUTH0_DOMAIN,
+      "auth0ClientId": process.env.AUTH0_CLIENT_ID,
+      "auth0Audience": process.env.AUTH0_AUDIENCE,
+      "apiURL": process.env.API_URL,
+    },
     "splash": {
       "image": "./assets/splash.png",
       "resizeMode": "contain",
@@ -15,16 +23,27 @@
       "**/*"
     ],
     "ios": {
-      "supportsTablet": true
+      "supportsTablet": true,
+      "bundleIdentifier": "com.auth0.rndemo"
     },
     "android": {
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
-      }
+      },
+      "package": "com.auth0.rndemo"
     },
     "web": {
       "favicon": "./assets/favicon.png"
-    }
+    },
+    "plugins": [
+      [
+        "react-native-auth0",
+        {
+          "domain": "dev-bajcmartinez.eu.auth0.com",
+          "customScheme": "auth0-rn-demo"
+        }
+      ]
+    ]
   }
-}
+};
